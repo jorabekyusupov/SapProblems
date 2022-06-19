712db383e912ad29d970cbe621ec4c7023b04d0d
Jo'rabek <60138404+jorabekyusupov@users.noreply.github.com>
2022-06-19 23:52:17 +0500

    update user index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -21,34 +21,43 @@
                         <td>{{ $user->firstname }}</td>
                         <td>{{ $user->lastname }}</td>
                         <td>{{ $user->email }}</td>
-                        <td class="{{$user->id === auth()->user()->id ? 'd-none' : ''}}">@if($user->employee)
+                        <td class="{{$user->id === auth()->user()->id || auth()->user()->id === 1 ? 'd-none' : ''}}">@if($user->employee)
                                 <form action="{{route('users.update', ["id"=> $user->id])}}" method="post">
                                     @method('put')
                                     @csrf
                                     <input type="hidden" name="employee" value="0">
-                                    <button  style="color: red;" type="submit" class="btn-user btn btn-danger">Ruxsatni ochirish</button>
+                                    <button style="color: red;" type="submit" class="btn-user btn btn-danger">Ruxsatni
+                                        ochirish
+                                    </button>
                                 </form>
                             @else
                                 <form action="{{route('users.update', ["id"=> $user->id])}}" method="post">
                                     @method('put')
                                     @csrf
                                     <input type="hidden" name="employee" value="1">
-                                    <button style="color: green;" type="submit" class="btn-user btn btn-success">Ruxsat berish</button>
+                                    <button style="color: green;" type="submit" class="btn-user btn btn-success">Ruxsat
+                                        berish
+                                    </button>
                                 </form>
                             @endif</td>
-                        <td class="{{$user->id === auth()->user()->id ? 'd-none' : ''}}" style="color: gray">@if($user->is_admin)
+                        <td class="{{$user->id === auth()->user()->id ? 'd-none' : ''}}"
+                            style="color: gray">@if($user->is_admin)
                                 <form action="{{route('users.update', ["id"=> $user->id])}}" method="post">
                                     @method('put')
                                     @csrf
                                     <input type="hidden" name="is_admin" value="0">
-                                    <button style="color: red;" type="submit" class="btn-user btn btn-danger">Ruxsatni ochirish</button>
+                                    <button style="color: red;" type="submit" class="btn-user btn btn-danger">Ruxsatni
+                                        ochirish
+                                    </button>
                                 </form>
                             @else
                                 <form action="{{route('users.update', ["id"=> $user->id])}}" method="post">
                                     @method('put')
                                     @csrf
                                     <input type="hidden" name="is_admin" value="1">
-                                    <button style="color: green; "  type="submit" class="btn-user btn btn-success">Ruxsat berish</button>
+                                    <button style="color: green; " type="submit" class="btn-user btn btn-success">Ruxsat
+                                        berish
+                                    </button>
                                 </form>
                             @endif</td>
                     </tr>
