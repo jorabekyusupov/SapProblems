27229273ea925aed6af40e05e365c4fcf0b3929f
Jo'rabek <60138404+jorabekyusupov@users.noreply.github.com>
2022-06-22 20:47:59 +0500

    nullable solution

--- database/migrations/2022_06_18_214832_create_problems_table.php
+++ database/migrations/2022_06_18_214832_create_problems_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('kod');
             $table->text('problem');
-            $table->text('solution');
+            $table->text('solution')->nullable();
             $table->dateTime('start_date');
             $table->dateTime('end_date');
             $table->unsignedBigInteger('user_id');
